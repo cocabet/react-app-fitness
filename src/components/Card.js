0b27530485f8d48fe16df252b0ebeb5bc7f546ea
Oscar <cocabet@gmail.com>
2020-02-28 11:22:30 -0600

    Se creo el component card

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import exerciseImg from '../images/exercise.png'
+import circleImg from '../images/circles.png'
+import './styles/Card.css'
+
+class Card extends Component{
+    render(){
+        const { title, description, img, leftColor, rightColor } = this.props
+        return(
+            <div className="card mx-auto Fitness-Card"
+            style={{
+                backgroundImage: `url(${circleImg}), linear-gradient(to right, ${rightColor},${leftColor})`
+            }}
+            >
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                        <img src ={img}/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        ) 
+    }
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//const element = document.createElement('h1');
+//element.innerText = 'Hello React'
+//const container = document.getElementById('root')
+//container.appendChild(element)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const container = document.getElementById('root')
+
+ReactDOM.render(<Card
+                    title="Technique Guides"
+                    description = "Learn Amazing street workout and calisthenics"
+                    img = "https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor = "#A74CF2"
+                    rightColor = "#617BFB"
+                />, container)
